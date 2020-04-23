3590c37c1a2777aad7801015b10bb6b936462344
getseto <getsee.to@gmail.com>
2020-04-22 19:33:13 -0500

    Profile and secure login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./Components/Profile";
+import history from "./Utils/history";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
